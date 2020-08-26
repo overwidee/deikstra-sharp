b1900e73a563f650ee75a58e44db7a287989594d
the-shichko <pashashichko0075@gmail.com>
2020-08-26 08:58:40 +0300

    [add] result console

--- examples/Program.cs
+++ examples/Program.cs
@@ -9,18 +9,20 @@ namespace examples
         {
             var deikstra = new Deikstra();
 
-            var mas = new decimal[4, 4]
+            deikstra.Matrix = new decimal[4, 4]
             {
                 { 0, 2, 5, 3 },
-                { 3, 0, 5, 2 }, 
+                { 3, 0, 5, 2 },
                 { 3, 0, 0, 4 },
-                { 3, 0, 0, 0 }, 
+                { 3, 0, 0, 0 },
             };
 
-            deikstra.QuantityVertex = 4;
-            deikstra.Matrix = mas;
-
             var result = deikstra.Start(0);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine($"Weight from 0 to {i} = {result[i]}");
+            }
             Console.ReadKey();
         }
     }
